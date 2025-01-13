cedc43188aa428865cf91fc7059f0814909941a8
Anukul (Personal) <anukulrthr259@gmail.com>
2025-01-13 15:59:06 +0530

    modified

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -3,6 +3,7 @@ import { IsAlphanumeric, IsAscii, IsNotEmpty, IsString } from 'class-validator';
 export class LoginDto {
   @IsString()
   @IsNotEmpty()
+  @IsAscii()
   username: string;
 
   @IsString()
