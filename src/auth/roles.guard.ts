49303e2554485858c134b933851e5c244cef6707
Anukul (Personal) <anukulrthr259@gmail.com>
2025-01-13 16:07:30 +0530

    Revised

--- src/auth/roles.guard.ts
+++ src/auth/roles.guard.ts
@@ -6,7 +6,7 @@ import { ROLES_KEY } from './roles.decorator';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  constructor(private reflector: Reflector ) {}
 
   canActivate(
     context: ExecutionContext,
